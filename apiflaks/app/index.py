7327d38aba664072753861672b77e6bc577afef5
robotumbel <ekoaripwinanto@gmail.com>
2018-12-09 22:18:23 +0700

    marge

--- apiflaks/app/index.py
+++ apiflaks/app/index.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-def index():
-    return 'Test Api'
+api.add_resource(HelloWorld, '/')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
